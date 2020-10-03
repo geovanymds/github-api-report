01ab0aff1129ce10915179b8d6519e0dd69ed653
Geovany <geovany.c.mendes@gmail.com>
2020-10-03 14:50:06 -0300

    Hide tokens

--- config/axios.js
+++ config/axios.js
@@ -1,8 +1,8 @@
 const config = {
   headers: {
-    //Authorization: "token 58f11a6525ff58320e2cd08aba478bd41068d4a0",
-    //Authorization: "token 83f7e6211a36762aa7fb94f5bfd5c3272a7c3dfe",
-    Authorization: "token 9d7fea2cbe3a8b65a174d87f0b8aabc39b64e792",
+    //Authorization: process.env.TOKEN_GEOVANY,
+    //Authorization: process.env.TOKEN_LEONARDO,
+    Authorization: process.env.TOKEN_THIAGO,
   },
 };
 
